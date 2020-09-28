f0b4b5715fac98657da8e9448aec461fe58bfa06
aidanmc95 <aidanmc95@gmail.com>
2020-09-28 16:14:46 -0700

    Grubhub Page Scraper

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-import requests
-import urllib.request
 import time
 import sys
 
-from requests_html import HTMLSession
 from bs4 import BeautifulSoup
-from grubhub import Grubhub
+# from grubhub import Grubhub
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -14,45 +11,55 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-url = 'https://www.grubhub.com/restaurant/okinawa-teriyaki-1100-western-ave-seattle/2064336'
-# url = 'https://www.brainyquote.com/'
+class Grubhub:
 
-chrome_driver_path = '/Users/aidanmuller-cohn/Development/code/Slack_Bot/Lets-Eat/chromedriver'
+    def __init__(self, url, webdriver):
+        self.info = False
+        self.url = url
+        self.webdriver = webdriver
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
+    def scraper(self):
+        url = self.url
+        print(url)
 
-webdriver = webdriver.Chrome(
-  executable_path=chrome_driver_path, options=chrome_options
-)
+        chrome_driver_path = '/Users/aidanmuller-cohn/Development/code/Slack_Bot/Lets-Eat/chromedriver'
 
-search_query = "life"
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
 
-if (len(sys.argv) >= 2):
-  search_query = sys.argv[1]
-  print(search_query)
+        webdriver = self.webdriver.Chrome(
+            executable_path=chrome_driver_path, options=chrome_options
+        )
+
+        search_query = "life"
+
+        if (len(sys.argv) >= 2):
+            search_query = sys.argv[1]
+            print(search_query)
 
 
-with webdriver as driver:
-    # Set timeout time 
-    wait = WebDriverWait(driver, 10)
+        with webdriver as driver:
+            # Set timeout time 
+            wait = WebDriverWait(driver, 10)
 
-    # retrive url in headless browser
-    driver.get(url)
-    
-    # find search box
-    # search = driver.find_element_by_id("hmSearch")
-    # search.send_keys(search_query + Keys.RETURN)
-    
-    wait.until(presence_of_element_located((By.CLASS_NAME, "menu-group")))
+            # retrive url in headless browser
+            driver.get(url)
+            
+            
+            wait.until(presence_of_element_located((By.CLASS_NAME, "menu-group")))
 
-    source = driver.page_source
+            source = driver.page_source
 
-    soup = BeautifulSoup(source)
+            soup = BeautifulSoup(source)
 
-    mydivs = soup.findAll('', {"class": "menuSection-title"})
+            mydivs = soup.findAll('', {"class": ["menuSection-title", 'menuItem-name']})
+            print(mydivs)
+            # must close the driver after task finished
+            driver.close()
+        return mydivs
 
-    print(mydivs)
 
-    # must close the driver after task finished
-    driver.close()
+url = 'https://www.grubhub.com/restaurant/wendys-2543-rainier-ave-s-seattle/1681402'
+
+grubhubTest = Grubhub(url, webdriver)
+grubhubTest.scraper()
--- grubhub.py
+++ grubhub.py
@@ -1,7 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
 class Grubhub:
 
-    def __init__(self):
+    def __init__(self, url):
         self.info = False
+        self.url = url
 
     def scraper(self):
-        return False
+        url = self.url
+        print(url)
+
+        chrome_driver_path = '/Users/aidanmuller-cohn/Development/code/Slack_Bot/Lets-Eat/chromedriver'
+
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+
+        webdriver = webdriver.Chrome(
+            executable_path=chrome_driver_path, options=chrome_options
+        )
+
+        search_query = "life"
+
+        if (len(sys.argv) >= 2):
+            search_query = sys.argv[1]
+            print(search_query)
+
+
+        with webdriver as driver:
+            # Set timeout time 
+            wait = WebDriverWait(driver, 10)
+
+            # retrive url in headless browser
+            driver.get(url)
+            
+            
+            wait.until(presence_of_element_located((By.CLASS_NAME, "menu-group")))
+
+            source = driver.page_source
+
+            soup = BeautifulSoup(source)
+
+            mydivs = soup.findAll('', {"class": ["menuSection-title", 'menuItem-name']})
+            print(mydivs)
+            # must close the driver after task finished
+            driver.close()
+        return mydivs
